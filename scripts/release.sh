e62264627dddff756d169f2fd2ab1088837570fe
Esko Luontola <esko.luontola@solita.fi>
2014-06-09 14:37:05 +0300

    Updated release scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,58 +1,63 @@
 #!/bin/bash
 set -eu
+: ${1:? Usage: $0 RELEASE_VERSION}
+SCRIPTS=`dirname "$0"`
 
-echo "Version number of the release to create, in MAJOR.MINOR.PATCH format: "
-read RELEASE_VERSION
+RELEASE_VERSION="$1"
 if [[ ! "$RELEASE_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "Error: invalid format"
+    echo "Error: RELEASE_VERSION must be in X.Y.Z format, but was $RELEASE_VERSION"
     exit 1
 fi
 
-echo "Version number of the next development increment, in MAJOR.MINOR.PATCH format: "
-read NEXT_VERSION
-if [[ ! "$NEXT_VERSION" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "Error: invalid format"
-    exit 1
-fi
+function contains-line() {
+    grep --line-regexp --quiet --fixed-strings -e "$1"
+}
+
+function demand-file-contains-line() {
+    local file="$1"
+    local expected="$2"
+    cat "$file" | contains-line "$expected" || (echo "Add this line to $file and try again:"; echo "$expected"; exit 1)
+}
+
+function assert-file-contains-substring() {
+    local file="$1"
+    local expected="$2"
+    cat "$file" | grep --quiet --fixed-strings -e "$expected" || (echo "Error: file $file did not contain $expected"; exit 1)
+}
+
+function set-project-version() {
+    local file="pom.xml"
+    local version="$1"
+    mvn versions:set \
+        -DgenerateBackupPoms=false \
+        -DnewVersion="$version" \
+        --file "$file"
+    assert-file-contains-substring "$file" "<version>$version</version>"
+}
+
+function next-snapshot-version() {
+    local prefix=`echo $1 | sed -n -r 's/([0-9]+\.[0-9]+\.)[0-9]+/\1/p'`
+    local suffix=`echo $1 | sed -n -r 's/[0-9]+\.[0-9]+\.([0-9]+)/\1/p'`
+    ((suffix++))
+    echo "$prefix$suffix-SNAPSHOT"
+}
+
+APP_NAME="JSON Migraine"
+NEXT_VERSION=`next-snapshot-version $RELEASE_VERSION`
+
+demand-file-contains-line README.md "### $APP_NAME $RELEASE_VERSION (`date --iso-8601`)"
 
 set -x
 
-
-# Require the release notes to contain an entry for the release version
-TODAY=`date --iso-8601=date`
-grep --fixed-strings --line-regexp --quiet "### JSON Migraine $RELEASE_VERSION ($TODAY)" RELEASE-NOTES.md
-
-
-# Release
-
-mvn versions:set \
-    -DgenerateBackupPoms=false \
-    -DnewVersion="$RELEASE_VERSION"
-
-git reset
-git add pom.xml RELEASE-NOTES.md
+set-project-version "$RELEASE_VERSION"
+git add -u
 git commit -m "Release $RELEASE_VERSION"
+git tag -s -m "$APP_NAME $RELEASE_VERSION" "v$RELEASE_VERSION"
 
-mvn clean deploy \
-    -Psonatype-oss-release
-mvn nexus:staging-close \
-    -Dnexus.description="JSON Migraine $RELEASE_VERSION"
+$SCRIPTS/stage.sh "$APP_NAME $RELEASE_VERSION"
 
-# Tag only after we know the release succeeded, so we can retry with
-# just a `git reset --hard HEAD^` without having to delete a tag also.
-git tag -s -m "Release $RELEASE_VERSION" "v$RELEASE_VERSION"
+set-project-version "$NEXT_VERSION"
+git add -u
+git commit -m "Prepare for next development iteration"
 
-
-# Next increment
-
-mvn versions:set \
-    -DgenerateBackupPoms=false \
-    -DnewVersion="$NEXT_VERSION-SNAPSHOT"
-
-git add pom.xml
-git commit -m "Prepare for next development increment"
-
-set +x
-echo "Done. Next steps:"
-echo "- Go to https://oss.sonatype.org/ and release the staging repository."
-echo "- git push; git push --tags"
+$SCRIPTS/publish.sh "$APP_NAME $RELEASE_VERSION"
